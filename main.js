5f880f0cf8e6f99a628f8a4e9214f679b82595e3
Drew Bratcher <drew@crowdspots.com>
2013-12-19 13:13:30 -0500

    fixed issue with html regexes after comments, also fixed an issue with close parens, closes #5

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ define(function (require, exports, module) {
     var LanguageManager = brackets.getModule("language/LanguageManager");
 
     var symbol_regex1 = /^(?:~|!|%|\^|\*|\+|=|:|;|,|\?|&|<|>|\|)/;
+    var symbol_regex2 = /^(=|\.|#|\|)/;
     var open_paren_regex = /^(\(|\[)/;
     var close_paren_regex = /^(\)|\])/;
     var keyword_regex1 = /^(if|else|return|var|function|include|doctype|each|mixin)/;
     var keyword_regex2 = /^(#|{|}|\.)/;
     var keyword_regex3 = /^(in)/;
-    var html_regex1 = /^(html|head|title|meta|link|script|body|br|div|input|span|a|img)/;
+    var html_regex1 = /^(html|head|title|meta|link|script|tr|td|body|br|div|input|span|a|img)/;
     var html_regex2 = /^(h1|h2|h3|h4|h5|p|strong|em)/;
     var comment_regex = /^(\/\/|\/\/-)/;
     
@@ -96,9 +97,12 @@ define(function (require, exports, module) {
                         stream.skipToEnd();
                         return "comment";
                     } 
-                    if(stream.match(html_regex1) || stream.match(html_regex2)) {
+                    if(stream.match(html_regex1) || stream.match(html_regex2)){
+                      if(stream.match(symbol_regex2)) {
                         state.justMatchedKeyword = true;
+                        stream.backUp(1);
                         return "variable";
+                      }
                     } 
                 } else if(stream.sol() && stream.match(comment_regex)) {
                     state.comment_indent = stream.indentation();
@@ -110,8 +114,11 @@ define(function (require, exports, module) {
                     stream.eatSpace();
                     return "keyword";
                 } else if(stream.sol() && (stream.match(html_regex1) || stream.match(html_regex2))) {
-                    state.justMatchedKeyword = true;
-                    return "variable";
+                    if(stream.match(symbol_regex2)) {
+                      state.justMatchedKeyword = true;
+                      stream.backUp(1);
+                      return "variable";
+                    }
                 } else if(stream.eatSpace()) {
                     state.justMatchedKeyword = false;
                     if(stream.match(keyword_regex3) && stream.eatSpace()) {
@@ -128,9 +135,9 @@ define(function (require, exports, module) {
                     state.justMatchedKeyword = true;
                     return "def";
                 } else if(stream.match(close_paren_regex)) { 
-                    state.justMatchedSymbol = true;
+                    state.justMatchedSymbol = false;
                     state.afterParen = false;
-                    state.justMatchedKeyword = true;
+                    state.justMatchedKeyword = false;
                     return "def";
                 } else if(stream.match(keyword_regex2)) {
                     state.justMatchedKeyword = true;
